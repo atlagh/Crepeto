b8c5c55503c3a0cec97dfe379cb2f0f7ebde87b9
amine <atlagh.amine@gmail.com>
2020-03-04 11:22:21 +0100

    Updates

---
+++ precache-manifest.a14587648dcb21de326d07af8ec35fa3.js
@@ -0,0 +1,86 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1ebdcc3946984d2cc5da76b96a4a3f9a",
+    "url": "/Crepeto/index.html"
+  },
+  {
+    "revision": "a2834bc5cda0b3798531",
+    "url": "/Crepeto/static/css/2.777f1ffd.chunk.css"
+  },
+  {
+    "revision": "071bee1191bd1448172f",
+    "url": "/Crepeto/static/css/main.314ee266.chunk.css"
+  },
+  {
+    "revision": "a2834bc5cda0b3798531",
+    "url": "/Crepeto/static/js/2.5097fec4.chunk.js"
+  },
+  {
+    "revision": "81c1ab2ba64ac9f85d481dd74a981dc6",
+    "url": "/Crepeto/static/js/2.5097fec4.chunk.js.LICENSE"
+  },
+  {
+    "revision": "071bee1191bd1448172f",
+    "url": "/Crepeto/static/js/main.3996fd9c.chunk.js"
+  },
+  {
+    "revision": "5f536acc3a7b756d2796",
+    "url": "/Crepeto/static/js/runtime-main.59e9a8c9.js"
+  },
+  {
+    "revision": "b72f72e7e1b4ea46767395f8c42beb9d",
+    "url": "/Crepeto/static/media/Background.b72f72e7.jpeg"
+  },
+  {
+    "revision": "5eaf0ec59b2ce33c997af36c20254ce2",
+    "url": "/Crepeto/static/media/MyMenu.5eaf0ec5.jpeg"
+  },
+  {
+    "revision": "0e82aaabb39af37defe72080ca20fe8f",
+    "url": "/Crepeto/static/media/caramelized.0e82aaab.png"
+  },
+  {
+    "revision": "213eff94c02efb83ab4a1b9bbeb317f2",
+    "url": "/Crepeto/static/media/chocolat.213eff94.png"
+  },
+  {
+    "revision": "bb7577d96f1e0f9c3e66ab4b9ead7e88",
+    "url": "/Crepeto/static/media/cocoa.bb7577d9.png"
+  },
+  {
+    "revision": "9b901b17d9733658ac6bacf31310cacb",
+    "url": "/Crepeto/static/media/ice.9b901b17.png"
+  },
+  {
+    "revision": "2492338c55b92f554eac4e246b07113f",
+    "url": "/Crepeto/static/media/logo.2492338c.png"
+  },
+  {
+    "revision": "42fd368317a4a8eb85d45cd2e32fe2a9",
+    "url": "/Crepeto/static/media/menu.42fd3683.png"
+  },
+  {
+    "revision": "20b07bc9f059285568c8951f6986b0e7",
+    "url": "/Crepeto/static/media/nutella.20b07bc9.png"
+  },
+  {
+    "revision": "997f9288492e6353e27d550c536caf36",
+    "url": "/Crepeto/static/media/peanut.997f9288.png"
+  },
+  {
+    "revision": "2a2862a60e59a0240237643a11d29ac1",
+    "url": "/Crepeto/static/media/plain.2a2862a6.png"
+  },
+  {
+    "revision": "ed43b366d56c2b8bfef4aaf1f7a1a303",
+    "url": "/Crepeto/static/media/strawberry.ed43b366.png"
+  },
+  {
+    "revision": "dd0e8da28a0d82e1dbde338322d8f36e",
+    "url": "/Crepeto/static/media/ultimate.dd0e8da2.png"
+  },
+  {
+    "revision": "254691f7bed226359bfb999615f52da0",
+    "url": "/Crepeto/static/media/waffle.254691f7.jpg"
+  }
+]);
